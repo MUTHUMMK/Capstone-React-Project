39fcfe552f64f54220e7403931a9ad4194ebdea2
MUTHUMMK <126318277+MUTHUMMK@users.noreply.github.com>
2023-09-12 11:54:35 +0530

    create.sh

--- create/create.sh
+++ create/create.sh
@@ -7,7 +7,7 @@ terraform init && terraform "${option}" --auto-approve
 sleep 6
 
 # to get public IP and store the variable 
-a=$(aws ec2 describe-instances --region ap-south-1 --filters "Name=tag:Name,Values=MMK" --query "Reservations].Instances[].PublicIpAddress" --output text )
+a=$(aws ec2 describe-instances --region ap-south-1 --filters "Name=tag:Name,Values=MMK" --query "Reservations[].Instances[].PublicIpAddress" --output text )
 
 b=$(aws ec2 describe-instances --region ap-south-1 --filters "Name=tag:Name,Values=MUTHU" --query "Reservations[].Instances[].PublicIpAddress" --output text )
 
